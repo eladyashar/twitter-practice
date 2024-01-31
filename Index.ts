56c28ab48c01c426152e20a722ef43c19e9ce12a
Elad Yashar <eladyashar1104@gmail.com>
2024-01-31 09:42:30 +0200

    changes for deployment

--- Index.ts
+++ Index.ts
@@ -4,12 +4,14 @@ import authRoute from './routes/authRoute'
 import usersRoute from './routes/usersRoute';
 import postsRoute from './routes/postsRoute';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(cors());
 
 app.use(activityLogger);
 app.use(express.json());
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 
 app.use("/auth", authRoute);
--- db.ts
+++ db.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '123456',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432
 });
 
 export default pool;
